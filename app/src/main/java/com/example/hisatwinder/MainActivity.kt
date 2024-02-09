ac6a7d9b5a5510b871875443854e3d4d85697bc5
Parbhat <parbhatjaiswal7@gmail.com>
2024-02-09 09:58:02 +0530

    yml updated 1.50

--- app/src/main/java/com/example/hisatwinder/MainActivity.kt
+++ app/src/main/java/com/example/hisatwinder/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.hisatwinder.ui.theme.HiSatwinderTheme
 
@@ -34,7 +35,7 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier.background()
+        modifier = modifier.background(Color.Red)
     )
 }
 
