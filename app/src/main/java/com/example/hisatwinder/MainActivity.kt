0c669c72f1c67f33b6c85e2eab1a0c81b02ec43c
Parbhat <parbhatjaiswal7@gmail.com>
2024-02-13 09:56:13 +0530

    yml updated 1.70

--- app/src/main/java/com/example/hisatwinder/MainActivity.kt
+++ app/src/main/java/com/example/hisatwinder/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Satwinder Singh Sher Gill")
+                    Greeting("Satwinder Singh Sher ss")
                 }
             }
         }
